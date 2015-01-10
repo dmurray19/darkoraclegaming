e7bc31aed3eca8c57d7f2d837b443563fbeae2c9
David Murray <dmurray19@gmail.com>
2015-01-10 12:44:45 -0600

    Account activations finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,33 +10,45 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
 
-  # Returns the hash digest of the given string.
+  # Returns the hash digest of the given string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  # Returns a random token.
+  # Returns a random token
   def User.new_token
     SecureRandom.urlsafe_base64
   end
 
-  # Remembers a user in the database for use in persistent sessions.
+  # Remembers a user in the database for use in persistent sessions
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # Returns true if the given token matches the digest
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
-  # Forgets a user.
+  # Forgets a user
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # Activates an account
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", 
-                     email: "user@example.com", 
-                     password: "foobar", 
+    @user = User.new(name: "Example User",
+                     email: "user@example.com",
+                     password: "foobar",
                      password_confirmation: "foobar")
   end
 
@@ -26,13 +26,13 @@ class UserTest < ActiveSupport::TestCase
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
-  end	
+  end
 
   test "email should not be too long" do
     @user.email = "a" * 256
     assert_not @user.valid?
   end
-	
+
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
@@ -60,8 +60,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-  
+
   test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
